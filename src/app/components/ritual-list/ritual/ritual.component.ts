499f269e02fe65c73fba4a68e004eb8ef44313e0
Jamie Ireland <jamieireland88@outlook.com>
2026-01-09 16:40:34 +0000

    feat: fixed scrolling and added drag handle

--- src/app/components/ritual-list/ritual/ritual.component.ts
+++ src/app/components/ritual-list/ritual/ritual.component.ts
@@ -1,10 +1,11 @@
 import { Component, HostBinding, inject, Input } from '@angular/core';
 import { Ritual } from '../../../models/models';
 import { Router } from '@angular/router';
+import { CdkDragHandle } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-ritual',
-  imports: [],
+  imports: [CdkDragHandle],
   templateUrl: './ritual.component.html',
   styleUrl: './ritual.component.scss'
 })
