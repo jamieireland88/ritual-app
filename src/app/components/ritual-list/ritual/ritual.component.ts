49120f6d895ea3d2d7b0c6bc22ffcf643e95d5fd
Jamie Ireland <jamieireland88@outlook.com>
2026-01-10 08:49:09 +0000

    feat: added gsap animations and haptics

--- src/app/components/ritual-list/ritual/ritual.component.ts
+++ src/app/components/ritual-list/ritual/ritual.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject, Input } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, inject, Input, ViewChild } from '@angular/core';
 import { Ritual, RitualType } from '../../../models/models';
 import { Router } from '@angular/router';
 import { CdkDragHandle } from '@angular/cdk/drag-drop';
+import { gsap } from 'gsap';
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
 
 @Component({
   selector: 'app-ritual',
@@ -10,11 +12,17 @@ import { CdkDragHandle } from '@angular/cdk/drag-drop';
   styleUrl: './ritual.component.scss'
 })
 export class RitualComponent {
+  @ViewChild('check') check!: ElementRef;
+  @ViewChild('fire') fire!: ElementRef;
+  @ViewChild('ritualEle') ritualEle!: ElementRef;
+
   @Input() public ritual!: Ritual;
 
   protected RitualType = RitualType;
 
   private readonly router = inject(Router);
+
+  private readonly cdRef = inject(ChangeDetectorRef);
 
   public view(): void {
     this.router.navigate(['/rituals/', this.ritual.id, encodeURI(this.ritual.name)]);
@@ -24,5 +32,16 @@ export class RitualComponent {
     event.stopImmediatePropagation();
     event.preventDefault();
     this.ritual.actioned = true;
+
+    this.cdRef.detectChanges();
+
+    gsap.fromTo(this.check.nativeElement, {scale: 0}, { scale: 1, duration: 1, ease: 'elastic.out' });
+    gsap.fromTo(this.fire.nativeElement, {scaleY: 1.4}, { scaleY: 1, duration: 0.5, ease: 'sine.out' });
+    gsap.timeline()
+    .to(this.ritualEle.nativeElement, {scale: 1.02, duration: 0.1})
+    .to(this.ritualEle.nativeElement, {scale: 1, duration: 0.1})
+    .play();
+    this.ritual.streak++;
+    Haptics.impact({ style: ImpactStyle.Heavy });
   }
 }
